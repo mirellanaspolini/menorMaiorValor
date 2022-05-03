11d22e514adb25be6132757079c588aff4d4bcc5
Mirella Naspolini <104786840+mirellanaspolini@users.noreply.github.com>
2022-05-03 14:50:31 -0300

    menor e maior valor

--- Exercicio.java
+++ Exercicio.java
@@ -1,4 +1,3 @@
-Faça um programa que receba dez números e armazene em uma lista. Em seguida percorra toda a lista e procure qual o MAIOR valor dentro da lista e qual o MENOR valor dentro da lista. No final apresente o MAIOR e o MENOR valor.
 import java.util.Scanner;
 
 public class Exercicio {
